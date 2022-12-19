d9d2f677cee82d08036e2836da47138ef68d78ad
deltanoru <116275620+deltanoru@users.noreply.github.com>
2022-12-19 20:13:08 +0300

    Delete player.py

--- data/player.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, obstacle_sprites):
-        super().__init__(groups)
-        self.image = pygame.image.load("data/character.png").convert_alpha()
-        self.rect = self.image.get_rect(topleft=pos)
