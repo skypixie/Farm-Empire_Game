6d3a8549737db5855acafd8e6bcd431cefa21f23
deltanoru <116275620+deltanoru@users.noreply.github.com>
2022-12-28 20:50:44 +0300

    Delete data directory

--- data/tilesets/Grass tiles v.2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Grass tiles v.2" tilewidth="16" tileheight="16" tilecount="77" columns="11">
- <image source="C:/Users/clean/Downloads/Sprout Lands - Sprites - Basic pack/Sprout Lands - Sprites - Basic pack/Tilesets/ground tiles/new tiles/Grass tiles v.2.png" width="176" height="112"/>
-</tileset>
