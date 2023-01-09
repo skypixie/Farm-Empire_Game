2989af2ea9a77a551bc116e7b9bb80aec4b62134
deltanoru <116275620+deltanoru@users.noreply.github.com>
2023-01-09 22:06:38 +0300

    Add files via upload

---
+++ graphics/tilemap/TilesetLogic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="TilesetLogic" tilewidth="64" tileheight="64" tilecount="80" columns="8">
+ <image source="../../../../graphics/Tilesets/TilesetLogic.png" width="512" height="640"/>
+</tileset>
